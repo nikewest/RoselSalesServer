b27bdf69a3479565a1d83cc2e4dc35ec8b195ed5
Nikiforov Nikita <nikiforovnikita@rosel.ru>
2017-06-06 14:06:18 +0300

    save settings rework

--- src/modules/serverlogic/RoselServerApplication.java
+++ src/modules/serverlogic/RoselServerApplication.java
@@ -1,12 +1,18 @@
 package modules.serverlogic;
 
-import roselsalesserver.UI.RoselServerPanel;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import roselsalesserver.ServerApplication;
 
 /**
  *
  * @author nikiforovnikita
  */
 public class RoselServerApplication {
+    
+    private static Logger LOG = Logger.getLogger(ServerApplication.class.getName());
     
     public static void main(String[] args) {
         RoselServerApplication app = new RoselServerApplication();
@@ -16,5 +22,16 @@ public class RoselServerApplication {
     void startApp(){
         RoselServerModel serverModel = new RoselServerModel();
         RoselServerController controller = new RoselServerController(serverModel);
+    }
+    
+    public static void initLogger() {
+        
+        try {
+            LogManager.getLogManager().readConfiguration(RoselServerApplication.class.getResourceAsStream("logging.properties"));            
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
     }
 }
